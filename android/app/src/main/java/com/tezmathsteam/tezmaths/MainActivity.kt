619184f05e9779fab103757ef20069c518d19639
Mitesh Goyal <miteshgoyal.00@gmail.com>
2025-06-16 15:30:25 +0530

    updates

--- android/app/src/main/java/com/tezmathsteam/tezmaths/MainActivity.kt
+++ android/app/src/main/java/com/tezmathsteam/tezmaths/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.miteshhq.tezmaths
+package com.tezmathsteam.tezmaths
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/tezmathsteam/tezmaths/MainApplication.kt
+++ android/app/src/main/java/com/tezmathsteam/tezmaths/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.miteshhq.tezmaths
+package com.tezmathsteam.tezmaths
 
 import android.app.Application
 import android.content.res.Configuration
